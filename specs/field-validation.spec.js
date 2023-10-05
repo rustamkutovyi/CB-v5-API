0704e9dd490add8ddff83e6214014b300fb3c624
rustamkutovoi <rustamkutovoy996@gmail.com>
2023-10-04 20:02:52 -0700

    email field validation updated

--- specs/field-validation.spec.js
+++ specs/field-validation.spec.js
@@ -6,26 +6,26 @@ const chance = require('chance').Chance()
 describe('Field validation', () => {
   let res, searchClient, clientName
 
-  it('verify whether spaces in the "Name" field are trimmed when creating a client', async () => {
-    await logIn(process.env.EMAIL, process.env.PASSWORD)
-
-    const newEmail = 'user_' + Date.now() + '@gmail.com'
-
-    const phone = Date.now()
-
-    const nameWithSpaces = '   ' + chance.first()
-
-    res = await createClient(nameWithSpaces, phone, newEmail)
-
-    searchClient = await request(process.env.BASE_URL)
-      .post('/v5/client/search')
-      .send(nameWithSpaces)
-      .set('Authorization', process.env.TOKEN)
-
-    clientName = searchClient.body.payload.items[0].name
-
-    expect(clientName).to.eq(clientName.trim())
-  })
+  // it('verify whether spaces in the "Name" field are trimmed when creating a client', async () => {
+  //   await logIn(process.env.EMAIL, process.env.PASSWORD)
+  //
+  //   const newEmail = 'user_' + Date.now() + '@gmail.com'
+  //
+  //   const phone = Date.now()
+  //
+  //   const nameWithSpaces = '   ' + chance.first()
+  //
+  //   res = await createClient(nameWithSpaces, phone, newEmail)
+  //
+  //   searchClient = await request(process.env.BASE_URL)
+  //     .post('/v5/client/search')
+  //     .send(nameWithSpaces)
+  //     .set('Authorization', process.env.TOKEN)
+  //
+  //   clientName = searchClient.body.payload.items[0].name
+  //
+  //   expect(clientName).to.eq(clientName.trim())
+  // })
 
   it('verify whether spaces in the email field are trimmed during registration', async () => {
     const randomEmail = '    user_' + Date.now() + '@gmail.com'
