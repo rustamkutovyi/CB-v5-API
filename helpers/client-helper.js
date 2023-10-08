fc401c558494038f24bc393a52fc99d884d55034
rustamkutovoi <rustamkutovoy996@gmail.com>
2023-10-07 18:47:43 -0700

    created test cases for client

--- helpers/client-helper.js
+++ helpers/client-helper.js
@@ -2,15 +2,30 @@ import request from 'supertest'
 
 const chance = require('chance').Chance()
 
-function createClient(
-  name = chance.first(),
-  phone = Date.now(),
-  email = 'user_' + Date.now() + '@gmail.com'
-) {
+function createClient(name = chance.first(), phone = Date.now()) {
   return request(process.env.BASE_URL)
     .post('/v5/client')
-    .send({ name, phone, email })
+    .send({ name, phone })
     .set('Authorization', process.env.TOKEN)
 }
 
-export { createClient }
+function searchClientByID() {
+  return request(process.env.BASE_URL)
+    .get('/v5/client/' + process.env.ID)
+    .set('Authorization', process.env.TOKEN)
+}
+
+function searchAllClients() {
+  return request(process.env.BASE_URL)
+    .post('/v5/client/search')
+    .set('Authorization', process.env.TOKEN)
+}
+
+function searchClientByName() {
+  return request(process.env.BASE_URL)
+    .post('/v5/client/search')
+    .send({ name: process.env.CLIENT_NAME })
+    .set('Authorization', process.env.TOKEN)
+}
+
+export { createClient, searchClientByID, searchAllClients, searchClientByName }
--- specs/client.spec.js
+++ specs/client.spec.js
@@ -1,20 +1,46 @@
 import { logIn } from '../helpers/general-helper'
-import { createClient } from '../helpers/client-helper'
+import {
+  createClient,
+  searchClientByID,
+  searchAllClients,
+  searchClientByName,
+} from '../helpers/client-helper'
 import { expect } from 'chai'
+import request from 'supertest'
 
 describe('Client', () => {
+  let response
   before(async () => {
     await logIn(process.env.EMAIL, process.env.PASSWORD)
   })
 
+  afterEach(async () => {})
+
   it('Create client', async () => {
-    const response = await createClient()
+    response = await createClient()
 
     await expect(response.statusCode).to.eq(200)
     await expect(response.body.message).to.eq('Client created')
   })
 
   it('Search client by ID', async () => {
-    await
-  });
+    response = await searchClientByID()
+
+    await expect(response.statusCode).to.eq(200)
+    await expect(response.body.message).to.eq('Get Client by id ok')
+  })
+
+  it('Search all clients', async () => {
+    response = await searchAllClients()
+
+    await expect(response.statusCode).to.eq(200)
+    await expect(response.body.message).to.eq('ClientSearch ok')
+  })
+
+  it('Search client by name', async () => {
+    response = await searchClientByName()
+
+    await expect(response.statusCode).to.eq(200)
+    await expect(response.body.message).to.eq('ClientSearch ok')
+  })
 })
