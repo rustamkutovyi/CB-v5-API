531a9809ec77e428a63e84fac78d4fc04eaff23a
rustamkutovoi <rustamkutovoy996@gmail.com>
2023-11-24 12:59:11 -0800

    negative tests for vendor.spec.js.spec.js have been updated

--- specs/vendor.spec.js
+++ specs/vendor.spec.js
@@ -8,6 +8,7 @@ import {
 } from '../helpers/vendor-helper'
 import { expect } from 'chai'
 import request from 'supertest'
+import { ignore } from '../.mocharc'
 
 const chance = require('chance').Chance()
 
@@ -165,6 +166,125 @@ describe('Vendor negative tests', () => {
     })
     it('Check body message', () => {
       expect(res.body.message).to.eq('Auth failed')
+    })
+  })
+
+  describe('Get all vendors without authorization', () => {
+    let res
+    before(async () => {
+      res = await request(process.env.BASE_URL).post('/v5/vendor/search')
+    })
+
+    it('Check status code', () => {
+      expect(res.statusCode).to.eq(400)
+    })
+    it('Check body message', () => {
+      expect(res.body.message).to.eq('Auth failed')
+    })
+  })
+
+  describe('Get vendor with not existing id', () => {
+    let res, vendorId
+    before(async () => {
+      vendorId = (await createVendor()).body.payload
+      await deleteVendor(vendorId)
+      res = await request(process.env.BASE_URL)
+        .get('/v5/vendor/' + vendorId)
+        .set('Authorization', process.env.TOKEN)
+    })
+
+    it('Check status code', () => {
+      expect(res.statusCode).to.eq(404)
+    })
+
+    it('Check body message', () => {
+      expect(res.body.message).to.eq('No vendor for provided id')
+    })
+  })
+
+  describe('Update vendor with not existing id', () => {
+    let vendorId, res
+    before(async () => {
+      vendorId = (await searchAllVendors()).body.payload.items[0]._id
+      await deleteVendor(vendorId)
+      res = await request(process.env.BASE_URL)
+        .patch('/v5/vendor/' + vendorId)
+        .send({ name: chance.name() })
+        .set('Authorization', process.env.TOKEN)
+    })
+    it('Check status code', () => {
+      expect(res.statusCode).to.eq(400)
+    })
+    it('Check body message', () => {
+      expect(res.body.message).to.eq('Vendor not found')
+    })
+  })
+
+  describe('Get vendor by name without authorization', () => {
+    let res, vendorName
+    before(async () => {
+      vendorName = (await searchAllVendors()).body.payload.items[0].name
+      res = await request(process.env.BASE_URL).post('/v5/vendor/search')
+    })
+    it('Check status code', () => {
+      expect(res.statusCode).to.eq(400)
+    })
+    it('Check body message', () => {
+      expect(res.body.message).to.eq('Auth failed')
+    })
+  })
+
+  describe('Verify that vendor name has not been changed without authorization', () => {
+    let res, vendorNameUpd, vendorId, vendorName
+    before(async () => {
+      vendorNameUpd = 'Tony'
+      vendorId = (await searchAllVendors()).body.payload.items[0]._id
+      res = await request(process.env.BASE_URL)
+        .patch('/v5/vendor/' + vendorId)
+        .send({ name: vendorName })
+      vendorName = (await searchVendorById(vendorId)).body.payload.name
+    })
+
+    it('Check status code', () => {
+      expect(res.statusCode).to.eq(400)
+    })
+    it('Check body message', () => {
+      expect(res.body.message).to.eq('Auth failed')
+    })
+    it('Verify that vendor name has not been updated', () => {
+      expect(vendorName).not.eq(vendorNameUpd)
+    })
+  })
+
+  describe('Delete vendor without auth', () => {
+    let res, vendorId
+    before(async () => {
+      vendorId = (await searchAllVendors()).body.payload.items[0]._id
+      res = await request(process.env.BASE_URL).delete('/v5/vendor/' + vendorId)
+    })
+
+    it('Check status code', () => {
+      expect(res.statusCode).to.eq(400)
+    })
+    it('Check body message', () => {
+      expect(res.body.message).to.eq('Auth failed')
+    })
+  })
+
+  describe('Delete vendor with not existing id', () => {
+    let res, vendorId
+    before(async () => {
+      vendorId = (await searchAllVendors()).body.payload.items[0]._id
+      await deleteVendor(vendorId)
+      res = await request(process.env.BASE_URL)
+        .delete('/v5/vendor/' + vendorId)
+        .set('Authorization', process.env.TOKEN)
+    })
+    it('Check status code', () => {
+      expect(res.statusCode).to.eq(400)
+    })
+    it('Check body message', () => {
+      expect(res.body.message).to.eq('Vendor not found')
     })
   })
 })
